bd56f8674148e377194e980bf6d2844ce91781d0
devunique83 <68051222+devunique83@users.noreply.github.com>
2022-03-22 18:57:21 +0530

    removed (#4)
    * removed
    
    * added comments only
    
    * removed lines

--- LearningPythonSelenium/action_chanins.py
+++ LearningPythonSelenium/action_chanins.py
@@ -2,15 +2,10 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-
-
-
-
 
 #added comment
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome() # to new driver object
 driver.get("https://www.facebook.com/")
 driver.maximize_window()
 ac = ActionChains(driver)
